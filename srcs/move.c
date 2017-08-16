e742dd1d732084d2a3e26f27fa6f4b9f8f4b5fd3
Mathilde YERNAUX <myernaux@e2r12p1.42.fr>
2017-08-16 11:49:06 +0200

    test mur

--- srcs/move.c
+++ srcs/move.c
@@ -6,7 +6,7 @@
 /*   By: tfaure <tfaure@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/23 11:12:38 by tfaure            #+#    #+#             */
-/*   Updated: 2017/08/16 10:00:51 by myernaux         ###   ########.fr       */
+/*   Updated: 2017/08/16 11:48:46 by myernaux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	move_forward(t_env *env)
 	double tmpx;
 	double tmpy;
 
-	tmpx = env->dat->px + 0.2 * cos(env->dat->alpha * M_PI / 180);
-	tmpy = env->dat->py + 0.2 * sin(env->dat->alpha * M_PI / 180) * -1;
+	tmpx = env->dat->px + 0.3 * cos(env->dat->alpha * M_PI / 180);
+	tmpy = env->dat->py + 0.3 * sin(env->dat->alpha * M_PI / 180) * -1;
 	if (env->dat->map[fl_to_int(tmpx)][fl_to_int(tmpy)] != '1')
 	{
-		env->dat->px += 0.2 * cos(env->dat->alpha * M_PI / 180);
-		env->dat->py += (0.2 * sin(env->dat->alpha * M_PI / 180)) * -1;
+		env->dat->px += 0.3 * cos(env->dat->alpha * M_PI / 180);
+		env->dat->py += (0.3 * sin(env->dat->alpha * M_PI / 180)) * -1;
 	}
 }
 
@@ -31,12 +31,12 @@ void	move_back(t_env *env)
 	double tmpx;
 	double tmpy;
 
-	tmpx = env->dat->px - 0.2 * cos(env->dat->alpha * M_PI / 180);
-	tmpy = env->dat->py - (0.2 * sin(env->dat->alpha * M_PI / 180)) * -1; 
+	tmpx = env->dat->px - 0.3 * cos(env->dat->alpha * M_PI / 180);
+	tmpy = env->dat->py - (0.3 * sin(env->dat->alpha * M_PI / 180)) * -1; 
 	if (env->dat->map[fl_to_int(tmpx)][fl_to_int(tmpy)] != '1')
 	{
-		env->dat->px -= 0.2 * cos(env->dat->alpha * M_PI / 180);
-		env->dat->py -= 0.2 * sin(env->dat->alpha * M_PI / 180) * -1;
+		env->dat->px -= 0.3 * cos(env->dat->alpha * M_PI / 180);
+		env->dat->py -= 0.3 * sin(env->dat->alpha * M_PI / 180) * -1;
 	}
 }
 
@@ -45,12 +45,12 @@ void	move_left(t_env *env)
 	double tmpx;
 	double tmpy;
 
-	tmpx = env->dat->py + 0.2 * cos(env->dat->alpha * M_PI / 180) * -1;
-	tmpy = env->dat->px + (0.2 * sin(env->dat->alpha * M_PI / 180)) * -1;
+	tmpy = env->dat->py + 0.3 * cos(env->dat->alpha * M_PI / 180) * -1;
+	tmpx = env->dat->px + (0.3 * sin(env->dat->alpha * M_PI / 180)) * -1;
 	if (env->dat->map[fl_to_int(tmpx)][fl_to_int(tmpy)] != '1')
 	{
-		env->dat->py += 0.2 * cos(env->dat->alpha * M_PI / 180) * -1;
-		env->dat->px += (0.2 * sin(env->dat->alpha * M_PI / 180)) * -1;
+		env->dat->px += (0.3 * sin(env->dat->alpha * M_PI / 180)) * -1;
+		env->dat->py += 0.3 * cos(env->dat->alpha * M_PI / 180) * -1;
 	}
 }
 
@@ -59,11 +59,11 @@ void	move_right(t_env *env)
 	double tmpx;
 	double tmpy;
 
-	tmpx = env->dat->py - 0.2 * cos(env->dat->alpha * M_PI / 180) * -1;
-	tmpy = env->dat->px - (0.2 * sin(env->dat->alpha * M_PI / 180)) * -1;
+	tmpy = env->dat->py - 0.3 * cos(env->dat->alpha * M_PI / 180) * -1;
+	tmpx = env->dat->px - (0.3 * sin(env->dat->alpha * M_PI / 180)) * -1;
 	if (env->dat->map[fl_to_int(tmpx)][fl_to_int(tmpy)] != '1')
 	{
-		env->dat->py -= 0.2 * cos(env->dat->alpha * M_PI / 180) * -1;
-		env->dat->px -= (0.2 * sin(env->dat->alpha * M_PI / 180)) * -1;
+		env->dat->px -= (0.3 * sin(env->dat->alpha * M_PI / 180)) * -1;
+		env->dat->py -= 0.3 * cos(env->dat->alpha * M_PI / 180) * -1;
 	}
 }
